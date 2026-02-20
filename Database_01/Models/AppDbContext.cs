ff1bdb8ef8faafb03479cc331cbf383f6468ce1c
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-02-20 15:56:06 +0630

    Feat : Add Database

---
+++ Database_01/Models/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_01.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Game> Games { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=JC_TUTO;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Game>(entity =>
+        {
+            entity.HasKey(e => e.GameId).HasName("PK__Games__2AB897FD19BA3041");
+
+            entity.Property(e => e.Genre).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Database_01/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database_01.Models;
+
+public partial class Game
+{
+    public int GameId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Genre { get; set; }
+
+    public int? Price { get; set; }
+
+    public DateOnly? ReleaseDate { get; set; }
+}
