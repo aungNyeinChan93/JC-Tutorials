6a5af0c384d19f3935b0474e8f08e6051fee6dcd
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-02-23 15:22:57 +0630

    Feat: update

--- Three.WebApi/EndPoints/BlogEndPoint.cs
+++ Three.WebApi/EndPoints/BlogEndPoint.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Database_02.Models;
+using Microsoft.AspNetCore.Mvc;
 using Three.WebApi.Services;
 
 namespace Three.WebApi.EndPoints
 {
-    public static class BlogEndPoint
+    public static class BlogEndPoint // presentation layer
     {
         public static WebApplication UseBlogs(this WebApplication app)
         {
@@ -19,6 +20,25 @@ namespace Three.WebApi.EndPoints
             {
                 var blog = blogService.GetOne(id);
                 return Results.Ok(blog);
+            }).WithName("Get-blog");
+
+            group.MapPost("/", ([FromServices]BlogService blogService, [FromBody]TblBlog blog) =>
+            {
+                var result = blogService.Create(blog);
+                return result is not null ? Results.CreatedAtRoute("Get-blog",new {id = result.BlogId },result) : Results.BadRequest();
+            });
+
+            group.MapPut("/{id:int}", ([FromServices]BlogService blogService, [FromRoute]int id, [FromBody]TblBlog blog) =>
+            {
+                var result = blogService.Update(id, blog);
+                return result is not null ? Results.Ok(result): Results.BadRequest();
+
+            });
+
+            group.MapDelete("/{id:int}", ([FromServices]BlogService blogService, [FromRoute]int id) =>
+            {
+                var result = blogService.Delete(id);
+                return result ? Results.NoContent() : Results.BadRequest();
             });
 
             return app;
--- Three.WebApi/Services/BlogService.cs
+++ Three.WebApi/Services/BlogService.cs
@@ -1,15 +1,16 @@
 ﻿using Database_02.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace Three.WebApi.Services
 {
-    public class BlogService
+    public class BlogService // Basiness layer 
     {
-        private readonly AppDbContext _db;
+        private readonly AppDbContext _db; // <== add db layer 
 
         public BlogService(AppDbContext db) // Constructor Dependency injection
         {
-            _db = db;
+            this._db = db;
         }
 
         public List<TblBlog>? GetAll()
@@ -22,6 +23,36 @@ namespace Three.WebApi.Services
         {
             var blog = _db.TblBlogs.AsNoTracking().FirstOrDefault(b => b.BlogId == id);
             return blog;
+        }
+
+        public TblBlog? Create(TblBlog blog)
+        {
+            _db.Add(blog);
+            var res = _db.SaveChanges();
+            return res >= 1? blog :null;
+        }
+
+        public TblBlog? Update(int id, TblBlog blog)
+        {
+            var updateBlog = _db.TblBlogs.AsNoTracking().FirstOrDefault(b => b.BlogId == id);
+
+            updateBlog!.Title = blog.Title;
+            updateBlog!.Description = blog.Description;
+            updateBlog!.AuthorName = blog.AuthorName;
+
+            _db.Entry(updateBlog).State = EntityState.Modified;
+            var res = _db.SaveChanges();
+            return res >= 1 ? updateBlog : null;
+        }
+
+        public bool Delete(int id)
+        {
+            var blog = _db.TblBlogs.AsNoTracking().FirstOrDefault(b => b.BlogId == id);
+            if(blog is null) return false;
+
+            _db.Entry(blog).State = EntityState.Deleted;
+            int res = _db.SaveChanges();
+            return res >= 1;
         }
     }
 }
