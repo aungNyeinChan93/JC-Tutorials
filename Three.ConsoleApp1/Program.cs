c7e42ffc3e0de8998e806017130a53769aa9bc03
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-02-25 14:16:14 +0630

    Feat: add service provider in console app by dependency injection

---
+++ Three.ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Database_02.Models;
+using five.WebApi.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+
+//new ServiceCollection().AddDbContext<AppDbContext>(())
+
+//var service = new ServiceCollection().AddSingleton<BlogService>().BuildServiceProvider();
+//var blogService = service.GetRequiredService<BlogService>();
+
+
+//var blogs = blogService.GetAllBlogs();
+
+//foreach (var blog in blogs!)
+//{
+//    Console.WriteLine($"Blog Title ==> {blog.Title}");
+//}
+
+
+
+var services = new ServiceCollection();
+
+// add services
+services.AddDbContext<AppDbContext>();
+services.AddSingleton<BlogService>();
+
+
+var provider = services.BuildServiceProvider();
+
+var blogService = provider.GetRequiredService<BlogService>();
+
+var blogs = blogService.GetAllBlogs();
+
+foreach (var blog in blogs!)
+{
+    Console.WriteLine($"Blog title ==>{blog.Title}");
+}
+
+
