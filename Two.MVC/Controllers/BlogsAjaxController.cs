b17060be1bb95645e46acdf7976b77b5c2c97bef
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-03-01 16:04:12 +0630

    feat: create

--- Two.MVC/Controllers/BlogsAjaxController.cs
+++ Two.MVC/Controllers/BlogsAjaxController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Database_02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks.Dataflow;
 using Three.WebApi.Services;
 
 namespace Two.MVC.Controllers
@@ -21,6 +23,18 @@ namespace Two.MVC.Controllers
         {
             var blogs = blogService.GetAll();
             return Json(blogs);
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Save(TblBlog blog)
+        {
+            var b = blogService.Create(blog);
+            return Json(b);   
         }
     }
 }
