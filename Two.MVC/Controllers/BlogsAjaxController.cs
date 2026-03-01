c7d9a3cf853ded8d2fab55a2efd4b60843370a1d
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-03-01 15:19:12 +0630

    feat: ajax and fetch

---
+++ Two.MVC/Controllers/BlogsAjaxController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Three.WebApi.Services;
+
+namespace Two.MVC.Controllers
+{
+    public class BlogsAjaxController : Controller
+    {
+        private readonly IBlogService blogService;
+
+        public BlogsAjaxController(IBlogService blogService)
+        {
+            this.blogService = blogService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAllBlogs()
+        {
+            var blogs = blogService.GetAll();
+            return Json(blogs);
+        }
+    }
+}
